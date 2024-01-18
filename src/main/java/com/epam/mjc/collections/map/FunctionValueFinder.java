62da64b00958e175f004bde81939b1bc3574ec82
Jahongir2109 <jahongirachilov004@gmail.com>
2024-01-18 13:46:25 +0500

    fixed all logics

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -1,13 +1,27 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
+        boolean state=false;
         Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
+        for (Integer value : functionMap.values()) {
+            if(value==requiredValue || value*5+2==requiredValue){
+                state=true;
+                break;
+            }
+        }
+        return state;
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
+        Map<Integer,Integer>integerMap=new HashMap<>();
+        for (Integer i : sourceList) {
+            integerMap.put(i,i);
+        }
+        return integerMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,64 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String,Integer> map=new TreeMap<>();
+        while (sentence!=""){
+            boolean state=false;
+           int index=sentence.indexOf(' ');
+           if(index<sentence.indexOf('.') && index!=-1){
+               if (index<sentence.indexOf(',') && sentence.indexOf(',')!=-1){
+
+               }else {
+                   if (sentence.indexOf(',')==-1){
+
+                   }else {
+                       index = sentence.indexOf(',');
+                   }
+               }
+           }
+           else{
+               index=sentence.indexOf('.');
+               if(index<sentence.indexOf(',')&&sentence.indexOf(',')!=-1){
+
+               }else {
+                   if (sentence.indexOf(',')==-1){
+
+                   }else {
+                       index = sentence.indexOf(',');
+                   }
+               }
+           }
+            if (index==0){
+                sentence=sentence.substring(1);
+            }else {
+                String word;
+                if (index != -1) {
+                    word = sentence.substring(0, index).toLowerCase();
+                    sentence = sentence.substring(index + 1);
+                } else {
+                    word = sentence.toLowerCase();
+                    sentence = "";
+                }
+                for (String key : map.keySet()) {
+                    if (key.equals(word)) {
+                        state = true;
+                        map.put(key, map.get(key) + 1);
+                    }
+                }
+                if (!state) {
+                    map.put(word, 1);
+                }
+                if (index == -1) {
+                    sentence = "";
+                    break;
+                }
+            }
+        }
+        return map;
     }
 }
